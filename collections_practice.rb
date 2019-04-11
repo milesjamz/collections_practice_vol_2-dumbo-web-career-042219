272408635ddaf095a9a295c254d26ee896ab2ac7
Miles Marillo <mjm03@hampshire.edu>
2019-04-11 16:04:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
 # your code goes here
+def begins_with_r(arguments)
+arguments.each do |word|
+  if word.start_with?("r")
+  return TRUE
+else
+  return FALSE
+end
+end
+end
+
+def contain_a(arguments)
+arguments.each do |word|
+newArray << word.include("a")
+return newArray
+end
+end
+
+def first_wa
+end
+
+def remove_non_strings
+end
+
+def count_elements
+end
+
+def merge_data
+end
+
+def find_cool
+end
+
+def organize_schools
+end
